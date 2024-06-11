b4fc5068fa90879b458eb0ad17769fe0bc1108d3
mattylumps <matt.lumpkin12@gmail.com>
2024-06-11 17:26:26 -0400

    fourth: almost there

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ def main():
     print(text)
     word_count = count_words(text)
     char_count = count_chars(text)
-    print(f"This text contains {word_count} words")
-    print(f"This text containts {char_count} characters")
+    sorted_list = sorting(char_count)
+    print(f"--- Begin report of books/frankenstein.txt ---\n {word_count} words found in the document\n {sorted_list}\n --- End report ---")
+    
 
 
 def get_book_text(path):
@@ -26,10 +27,22 @@ def count_chars(text):
     for i in lower_case:
         if i not in char_counter_dict:
             count_for_char = 1
-            char_counter_dict.update({i:count_for_char})
+            
         if i in char_counter_dict:
             count_for_char += 1
-            char_counter_dict.update({i:count_for_char})
+        char_counter_dict.update({i:count_for_char})
     return char_counter_dict
+
+def sorting(char_counter_dict):
+    dict_list = []
+    for key, value in char_counter_dict.items():
+        if key.isalpha():
+            dict_list.append({"char": key, "num": value})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def sort_on(dictionary):
+    return dictionary["num"]
+    
 
 main()
